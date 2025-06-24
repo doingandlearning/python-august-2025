eab0477aa9ee4d25eb2384d165ce6ef59d2df2e0
Kevin Cunningham <kevin@kevincunningham.co.uk>
2025-06-24 11:19:41 +0100

    adds more code

---
+++ actual-code/04-data-containers/04-nested-lists.py
@@ -0,0 +1,16 @@
+bands = [
+    ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"],  # The Beatles
+    ["Freddie Mercury", "Brian May", "Roger Taylor", "John Deacon"],      # Queen
+    ["Kurt Cobain", "Krist Novoselic", "Dave Grohl"],                     # Nirvana
+    ["Mick Jagger", "Keith Richards", "Charlie Watts", "Ronnie Wood"],   # The Rolling Stones
+    ["Beyoncé", "Kelly Rowland", "Michelle Williams"],                   # Destiny's Child
+    ["Thom Yorke", "Jonny Greenwood", "Colin Greenwood", "Ed O'Brien", "Phil Selway"],  # Radiohead
+    ["Bono", "The Edge", "Adam Clayton", "Larry Mullen Jr."],            # U2
+    ["Chris Martin", "Guy Berryman", "Jonny Buckland", "Will Champion"], # Coldplay
+    ["Debbie Harry", "Chris Stein", "Clem Burke", "Jimmy Destri"],       # Blondie
+    ["Jack White", "Meg White"]                                          # The White Stripes
+]
+
+print(bands[0][3])
+print(bands[5][1])
+print(bands[4][0])
