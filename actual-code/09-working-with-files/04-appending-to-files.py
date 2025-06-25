b32807ba8cb8d4836df75ccdf34e04ca4e9f2bce
Kevin Cunningham <kevin@kevincunningham.co.uk>
2025-06-25 15:00:13 +0100

    adds data

--- actual-code/09-working-with-files/04-appending-to-files.py
+++ actual-code/09-working-with-files/04-appending-to-files.py
@@ -1,7 +1,12 @@
-import datetime
+# import datetime
+from datetime import datetime  # datetime -> we want datetime
+import os
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+os.chdir(script_dir)
 
 try:
   with open("time.log", "a") as file:
-    file.write(f"{datetime.datetime.now()}: This is a log message.\n")
+    file.write(f"{datetime.now()}: This is a log message.\n")
 except:
   print("Problem opening or writing to file.")
